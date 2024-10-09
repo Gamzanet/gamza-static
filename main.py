84abc3f35a25fd3c0801f5c4f696d1ea570d7e32
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-09 13:24:34 +0900

    ✨ Feat : info-function
    Signed-off-by: Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ def is_valid_hook(_target_path="source/1.sol"):
     return True
 
 
+# currently source code should include single contract
+def get_function_info(_target_path="source/3.sol"):
+    # Get the output of the semgrep command
+    output: list = get_semgrep_output("info-function", _target_path)
+    # pprint(output)
+
+    # [*].data.CONTRACT
+    # [*].data.SIG
+    # [*].data.VIS
+    # [*].data.PAY
+    # [*].data.PURITY
+    # [*].data.MOD
+    # [*].data.RETURN
+    res = list(search("[*].data.[*][0]", output))
+    # pprint(res)
+    return res
+
+
 if __name__ == "__main__":
-    res = is_valid_hook(_target_path="source/1.sol")
-    print(res)
+    # res = is_valid_hook(_target_path="source/1.sol")
+    res = get_function_info(_target_path="source/2.sol")
+    pprint(res)
