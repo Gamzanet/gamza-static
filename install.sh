92d10c5367325cd84f5be75fea1abb04e7a72361
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-13 14:22:59 +0900

    Revert "🚧 WIP : slither sample"
    This reverts commit 8c1fe065f176b8c5c0dfad69cac3b1ee644ba7db.

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ function main() {
     echo "install start"
     pip install --upgrade pip -q
     pip install -U semgrep -q
+    pip install -U web3 -q
     pip install -U python-dotenv -q
     pip install -U jmespath -q
     pip install -U PyYAML -q
