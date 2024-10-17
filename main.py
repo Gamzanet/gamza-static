424061b562c9d75647b8df27fe51d40914463d12
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-17 22:54:34 +0900

    🐛 Fix : Test for Hook
    Signed-off-by: Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os.path
+from pprint import pprint
+
 from engine import layer_0
 from etherscan.unichain import store_foundry_toml, store_remappings, store_all_dependencies, unichain_dir
 from parser.layer_2 import get_variables
@@ -12,26 +15,29 @@ def test_integration():
     store_remappings(_address)
     store_foundry_toml()
 
-    _base_dir = unichain_dir
-    layer_0.format_code(_base_dir)
+    layer_0.format_code(unichain_dir)  # "code/unichain" directory
 
     # linting the target contract recursively lints all dependencies
     res: list[str] = layer_0.compile_slither(_paths[0])
     print(res)
 
+    # to run semgrep rules,
+    # path needs to start with "code"
+    _target_path = os.path.join(unichain_dir, _paths[0])
+
     _output_l: list = get_semgrep_output(
         "misconfigured-Hook",
-        _paths[0],
+        _target_path,
         False
     )
-    print(_output_l)
+    pprint(_output_l)
 
     _output_l: list = get_semgrep_output(
         "info-variable",
-        _paths[0],
+        _target_path,
         False
     )
-    print(_output_l)
+    pprint(_output_l)
 
-    _output_d: dict = get_variables(_paths[0])
-    print(_output_d)
+    _output_d: dict = get_variables(_target_path)
+    pprint(_output_d)
