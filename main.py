339924492644242d6f6bc49c05ca90ac9b95ae16
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-21 13:01:23 +0900

    💡 Comment : add description
    Signed-off-by: Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# to run `python main.py` in root dir, add path of library to sys.path
 _path_lib = os.path.abspath(os.path.join(os.path.dirname(__file__), "lib"))
 sys.path.append(_path_lib)
 
