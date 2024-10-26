4ca241aa37c7d273e46193ab48f7ce6db6d2eb94
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-26 16:13:09 +0900

    ✅ Test : Unit Test Frame
    - function / variable

--- layers/Builder.py
+++ layers/Builder.py
@@ -1,7 +1,9 @@
+import os.path
 from typing import Generic, TypeVar
 
 from engine.layer_0 import set_solc_version_by_content
 from layers.Loader import Loader
+from layers.dataclass.Attributes import Mutability, Scope, Location, Visibility
 from layers.dataclass.Components import Contract, Function, Variable
 from parser.layer_2 import get_inheritance, get_library, get_contract_name
 
@@ -36,33 +38,205 @@ class FunctionBuilder(BaseBuilder[Function]):
     def __init__(self):
         super().__init__()
 
-    def build(self, code) -> Function:
-        raise NotImplementedError
+    def build(self, code) -> list[Function]:
+        return [
+            Function(
+                name="setValue",
+                parameters=[],
+                visibility="public",
+                modifiers=["aa", "b(this)"],
+                is_override=True,
+                mutability="",
+                returns=[],
+                body="""
+        require(_value > 0, "Value must be greater than zero");
+        assert(value != _value);
+        require(isEven(_value), "Value must be even");
+        if (_value < 10) {
+            revert ValueTooLow({provided: _value, required: 10});
+        }
+        revert();
+        revert("This is a revert without a custom error");
+        value = _value;""".strip(),
+                _has_low_level_call=True
+            ),
+            Function(
+                name="isEven",
+                parameters=[],
+                visibility="internal",
+                modifiers=[],
+                is_override=False,
+                mutability="pure",
+                returns=[],
+                body="return _value % 2 == 0;",
+                _has_low_level_call=False
+            )
+        ]
 
 
 class VariableBuilder(BaseBuilder[Variable]):
     def __init__(self):
         super().__init__()
 
-    def build(self, code) -> Variable:
-        raise NotImplementedError
+    def build(self, code) -> list[Variable]:
+        return [
+            Variable(
+                name="value",
+                signature="ComplexChecks",
+                type="uint256",
+                location=Location.STORAGE,
+                visibility=Visibility.PUBLIC,
+                scope=Scope.STORAGE,
+                mutability=Mutability.MUTABLE
+            ),
+            Variable(
+                name="_value",
+                signature="ComplexChecks:setValue",
+                type="bytes",
+                location=Location.CALLDATA,
+                visibility=Visibility.PUBLIC,
+                scope=Scope.ARGS,
+                mutability=Mutability.IMMUTABLE
+            ),
+            Variable(
+                name="result",
+                signature="ComplexChecks:setValue",
+                type="bytes",
+                location=Location.MEMORY,
+                visibility=Visibility.INTERNAL,
+                scope=Scope.RETURNS,
+                mutability=Mutability.MUTABLE
+            ),
+            Variable(
+                name="_value",
+                signature="ComplexChecks:isEven",
+                type="uint256",
+                location=Location.MEMORY,
+                visibility=Visibility.INTERNAL,
+                scope=Scope.ARGS,
+                mutability=Mutability.MUTABLE
+            ),
+            Variable(
+                name="result",
+                signature="ComplexChecks:isEven",
+                type="bool",
+                location=Location.MEMORY,
+                visibility=Visibility.INTERNAL,
+                scope=Scope.RETURNS,
+                mutability=Mutability.MUTABLE
+            )
+        ]
+
+
+_target_code: str = """// SPDX-License-Identifier: MIT
+            pragma solidity ^0.8.0;contract ComplexChecks is A, B { using aa for *; uint256 public value; error ValueTooLow(uint256 provided, uint256 required);constructor(uint256 _initialValue) {      value = _initialValue;}function setValue(uint256 _value) public aa b(this) override {require(_value > 0, "Value must be greater than zero"); assert(value != _value); require(isEven(_value), "Value must be even"); if (_value < 10) {revert ValueTooLow({provided: _value, required: 10});} revert();     revert("This is a revert without a custom error"); value = _value;}function isEven(uint256 _value) internal pure returns (bool) {return _value % 2 == 0;}}"""
+_cached = "49cd11d435ac68a380b5c14dd54b9720fa27bbd4250adb9b1136c7fa4673e784.sol"
+_cached_path = BaseBuilder().loader.as_abs_path(os.path.join("out", _cached))
 
 
 def test_contract_builder():
-    target_code: str = """// SPDX-License-Identifier: MIT
-            pragma solidity ^0.8.0;contract ComplexChecks is A, B { using aa for *; uint256 public value; error ValueTooLow(uint256 provided, uint256 required);constructor(uint256 _initialValue) {      value = _initialValue;}function setValue(uint256 _value) public {require(_value > 0, "Value must be greater than zero"); assert(value != _value); require(isEven(_value), "Value must be even"); if (_value < 10) {revert ValueTooLow({provided: _value, required: 10});} revert();     revert("This is a revert without a custom error"); value = _value;}function isEven(uint256 _value) internal pure returns (bool) {return _value % 2 == 0;}}"""
     builder = ContractBuilder()
-    _path = builder.loader.cache_content(target_code, "sol")
-    build = builder.build(_path)
-    assert build.target_code == target_code
-    assert build.version == "0.8.0"
-    assert build.name == "ComplexChecks"
-    assert build.inheritance == ["A", "B"]
-    assert build.library == ["aa"]
-    assert build == Contract(
-        target_code=target_code,
+    _path = _cached_path
+    if not os.path.exists(_path):
+        _path = builder.loader.cache_content(_target_code, "sol")
+    assert builder.build(_path) == Contract(
+        target_code=_target_code,
         version="0.8.0",
         name="ComplexChecks",
         inheritance=["A", "B"],
         library=["aa"]
     )
+
+
+def test_function_builder():
+    builder = FunctionBuilder()
+    _path = _cached_path
+    if not os.path.exists(_path):
+        _path = builder.loader.cache_content(_target_code, "sol")
+    assert builder.build(_path) == [
+        Function(
+            name="setValue",
+            parameters=[],
+            visibility="public",
+            modifiers=["aa", "b(this)"],
+            is_override=True,
+            mutability="",
+            returns=[],
+            body="""
+        require(_value > 0, "Value must be greater than zero");
+        assert(value != _value);
+        require(isEven(_value), "Value must be even");
+        if (_value < 10) {
+            revert ValueTooLow({provided: _value, required: 10});
+        }
+        revert();
+        revert("This is a revert without a custom error");
+        value = _value;""".strip(),
+            _has_low_level_call=True
+        ),
+        Function(
+            name="isEven",
+            parameters=[],
+            visibility="internal",
+            modifiers=[],
+            is_override=False,
+            mutability="pure",
+            returns=[],
+            body="return _value % 2 == 0;",
+            _has_low_level_call=False
+        )
+    ]
+
+
+def test_variable_builder():
+    builder = VariableBuilder()
+    _path = _cached_path
+    if not os.path.exists(_path):
+        _path = builder.loader.cache_content(_target_code, "sol")
+    assert builder.build(_path) == [
+        Variable(
+            name="value",
+            signature="ComplexChecks",
+            type="uint256",
+            location=Location.STORAGE,
+            visibility=Visibility.PUBLIC,
+            scope=Scope.STORAGE,
+            mutability=Mutability.MUTABLE
+        ),
+        Variable(
+            name="_value",
+            signature="ComplexChecks:setValue",
+            type="bytes",
+            location=Location.CALLDATA,
+            visibility=Visibility.PUBLIC,
+            scope=Scope.ARGS,
+            mutability=Mutability.IMMUTABLE
+        ),
+        Variable(
+            name="result",
+            signature="ComplexChecks:setValue",
+            type="bytes",
+            location=Location.MEMORY,
+            visibility=Visibility.INTERNAL,
+            scope=Scope.RETURNS,
+            mutability=Mutability.MUTABLE
+        ),
+        Variable(
+            name="_value",
+            signature="ComplexChecks:isEven",
+            type="uint256",
+            location=Location.MEMORY,
+            visibility=Visibility.INTERNAL,
+            scope=Scope.ARGS,
+            mutability=Mutability.MUTABLE
+        ),
+        Variable(
+            name="result",
+            signature="ComplexChecks:isEven",
+            type="bool",
+            location=Location.MEMORY,
+            visibility=Visibility.INTERNAL,
+            scope=Scope.RETURNS,
+            mutability=Mutability.MUTABLE
+        )
+    ]
