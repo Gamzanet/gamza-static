f6f9f8aae682f6f77c051b6495588ff42b1860e0
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-27 23:34:25 +0900

    ✅ Test : Aggregator

---
+++ code/DoubleInitHook.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract DoubleInitHook is BaseHook {
+
+    address public hookOperator;
+    uint256 public maxSwapCounter;
+    mapping(PoolId => uint256) public swapCounter;
+
+    constructor(IPoolManager _manager) BaseHook(_manager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: true,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function beforeInitialize(
+        address sender, /* sender **/
+        PoolKey calldata, /* key **/
+        uint160, /* sqrtPriceX96 **/
+        bytes calldata /* hookData **/
+    ) external override returns (bytes4) {
+        require(hookOperator == address(0), "DoubleInitHook: already initialized");
+        hookOperator = sender;
+        return this.beforeInitialize.selector;
+    }
+
+    function beforeSwap(
+        address, /* sender **/
+        PoolKey calldata key, /* key **/
+        IPoolManager.SwapParams calldata, /* params **/
+        bytes calldata /* hookData **/
+    ) external override returns (bytes4, BeforeSwapDelta, uint24) {
+        swapCounter[key.toId()] += 1;
+        if (swapCounter[key.toId()] > maxSwapCounter) {
+            maxSwapCounter = swapCounter[key.toId()];
+        }
+        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+
+
+    function withdrawAll() external payable {
+        require(msg.sender == hookOperator, "DoubleInitHook: not operator");
+        (bool success,) = payable(hookOperator).call{value: address(this).balance}("");
+        require(success, "DoubleInitHook: withdraw failed");
+    }
+
+    receive() external payable {}
+
+}
